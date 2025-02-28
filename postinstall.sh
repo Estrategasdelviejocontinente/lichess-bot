8e83c849ecc1bb8652e73ad49f1d3f9144762a19
Estrategasdelviejocontinente <09rolu157@gmail.com>
2025-02-28 09:30:09 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pip3 install -r requirements.txt
 
+# Crear un entorno virtual en .venv
+python3 -m venv .venv
+
+# Activar el entorno virtual
+source .venv/bin/activate
+
+# Actualizar pip dentro del venv
+pip install --upgrade pip
+
+# Instalar dependencias en el entorno virtual
+pip install -r requirements.txt
